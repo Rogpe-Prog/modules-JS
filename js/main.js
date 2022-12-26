d2d38158fb115e082c6dd57aec50c59d507dc252
Rogpe-Prog <paulo.rogpe@gmail.com>
2022-12-26 08:08:59 -0300

    using module JS

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//import * as nb from './numbers.js';
+
+import { sum } from './numbers.js';
+import Product from './product.js';
+
+
+const x = sum(3, 4);
+
+console.log(x);
+
+const obj = new Product("Computer", 800.0, 10);
+
+console.log(obj);
+
